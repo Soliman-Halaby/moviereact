1d9ed2dde797d519b63c392b27d904b841223451
Rob1 <60504992+RobinB-dev@users.noreply.github.com>
2021-03-19 21:06:40 +0100

    :notebook: Add image

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './header.css';
+
+// import logo from '../../assets/logos/logo.png;';
 // import iconHeart from './images/heart-red.svg';
 
 function Header(props) {
     return (
         <div className="header">
-            <div className="image">
+            <div className="headerContainer">
+                <div className="logo">
+                    <img src='../../assets/logos/logo.png'></img>
+                </div>
+                <h2>{props.note}</h2>
             </div>
-            <h2>{props.note}</h2>
+
         </div>
     );
 }
