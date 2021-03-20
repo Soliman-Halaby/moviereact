66cc4be37af7da799a312b0332f1667010b82cb1
valen <valentin.houplain@hetic.net>
2021-03-20 02:07:49 +0100

    :rocket: video component added to film page

--- src/component/Pages/Film.js
+++ src/component/Pages/Film.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Header from "../header/header";
+import Videodisplay from "../VideoDisplay/Videodisplay";
+import video1 from "../../assets/videos/banana.mp4";
+
 
 export default function Film() {
   return (
     <div className="containerFilm">
       <Header />
+      <Videodisplay
+      videoSrc = {video1}/>
     </div>
   );
 }
