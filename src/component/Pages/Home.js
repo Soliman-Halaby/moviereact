f0c3bdcac44925259c4a74af7e0b92b1cbcc88dc
Soliman Al Halaby <60599592+Soliman-Halaby@users.noreply.github.com>
2021-03-20 02:06:51 +0100

    :rocket: Adding features to home page

--- src/component/Pages/Home.js
+++ src/component/Pages/Home.js
@@ -2,12 +2,50 @@ import React from "react";
 import Header from "../header/header";
 import Videotitle from "../videoTitle/Videotitle";
 
+import "./Home.css";
+
 export default function Home() {
   return (
     <div className="containerHome">
       <Header />
-      <div className="videoWrapper">
-        <Videotitle />
+      <div className="filmPart">
+        <span className="titleCategorie">Nouveauté</span>
+        <div className="videoWrapper">
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+        </div>
+      </div>
+      <div className="filmPart">
+        <span className="titleCategorie">Top 10</span>
+        <div className="videoWrapper">
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+        </div>
+      </div>
+      <div className="filmPart">
+        <span className="titleCategorie">Populaire</span>
+        <div className="videoWrapper">
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+          <Videotitle />
+        </div>
       </div>
     </div>
   );
