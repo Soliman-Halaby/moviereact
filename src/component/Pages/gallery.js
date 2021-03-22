a99172fec4bd2a473eb5db1a2cc792cf89b2dd30
Grégoire <gregoire.richard@hetic.net>
2021-03-22 13:51:19 +0100

    :lipstick: Alice Carousel dots and arrows removal
    comments added too :')

--- src/component/Pages/gallery.js
+++ src/component/Pages/gallery.js
@@ -6,9 +6,9 @@ import Videotitle from "../videoTitle/Videotitle";
 // alice carousel doc here : https://github.com/maxmarinich/react-alice-carousel
 const handleDragStart = (e) => e.preventDefault();
 const responsive = {
-    0: { items: 1 },
-    568: { items: 2 },
-    1024: { items: 3 },
+    0: { items: 2 },
+    568: { items: 3 },
+    1024: { items: 4 }, // number of items per resolution 
 };
 const items = [
   <Videotitle onDragStart={handleDragStart} />,
@@ -22,12 +22,14 @@ const items = [
   <Videotitle onDragStart={handleDragStart} />
   //Videotitle component is stored in the carousel
 ];
-const Carousel = () => (
+const Carousel = () => ( // any modification of the alice carousel goes here
     <AliceCarousel
         mouseTracking
         items={items}
         responsive={responsive}
-    />
+        disableButtonsControls ={true}
+        disableDotsControls = {true}
+    /> 
 );
 
 export default Carousel;
