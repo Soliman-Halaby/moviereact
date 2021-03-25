41366cb76744becc76815027d32ee703d85930db
Grégoire <gregoire.richard@hetic.net>
2021-03-25 16:19:40 +0100

    :sparkles: Search engine
    introduction of the search engine

--- src/component/SearchBar/SearchBar.js
+++ src/component/SearchBar/SearchBar.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Loupe from "../../assets/logos/loupe.svg";
-
+import SearchEngine from "../searchEngine/searchEngine.js";
 import "./SearchBar.css";
 
-const SearchBar = () => {
-    return(
+class SearchBar extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = { 
+          search: '',
+          formatedString: ''
+        }
+    }
+    
+    changeSearchStatement = (event) => {
+        this.setState({
+            search: event.target.value.replace(' ', '+')
+        })
+    }
+
+    render() {
+      console.log(SearchEngine(this.state.search));
+      return (
         <React.Fragment>
             <div className="inputContainer">
                 <img className="iconLoupe" src={Loupe} alt="loupe"></img>
@@ -14,12 +30,15 @@ const SearchBar = () => {
                     class="form__field"
                     placeholder="Name"
                     id="name"
-                    name="name"
+                    name="search"
+                    type="text"
+                    onChange={this.changeSearchStatement}
                     ></input>
                 </div>
             </div>
         </React.Fragment>
-    )
-}
-
+        
+      )
+    }
+  }
 export default SearchBar;
