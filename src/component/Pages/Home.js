8d4df2dd29540343fd3de783f8a350bda02a49a7
Soliman Al Halaby <60599592+Soliman-Halaby@users.noreply.github.com>
2021-03-26 16:34:16 +0100

    :lipstick: Fix Casting Responsive

--- src/component/Pages/Home.js
+++ src/component/Pages/Home.js
@@ -1,51 +1,35 @@
 import React from "react";
 import Header from "../header/header";
 import Footer from "../Footer/footer";
-import Carousel from "../Carousel/Carousel"
+import Carousel from "../Carousel/Carousel";
 import Genres from "../header/Genres";
 import Search from "../header/Search";
 import "./Home.css";
 
 export default function Home() {
-  
   return (
     <div className="containerHome">
-      <Header 
-        // props={}
+      <Header
+      // props={}
       />
       {/* <Search /> */}
       <Genres />
-      
-      <div className="filmPart">
-        <span className="titleCategorie">Recherche</span>
-          <Carousel 
-            sortBy= {"upcoming"}
-          />
-        <div className="videoWrapper">
-        </div>
-      </div>
+
       <div className="filmPart">
         <span className="titleCategorie">Nouveauté</span>
-          <Carousel 
-            sortBy= {"upcoming"}
-          />
-        <div className="videoWrapper">
-        </div>
+        <Carousel sortBy={"upcoming"} />
+        <div className="videoWrapper"></div>
       </div>
       <div className="filmPart">
         <span className="titleCategorie">Top 10</span>
         <div className="videoWrapper">
-          <Carousel 
-            sortBy= {"top_rated"}
-          />
+          <Carousel sortBy={"top_rated"} />
         </div>
       </div>
       <div className="filmPart">
         <span className="titleCategorie">Populaire</span>
         <div className="videoWrapper">
-          <Carousel 
-            sortBy= {"popular"}
-          />
+          <Carousel sortBy={"popular"} />
         </div>
       </div>
       <Footer />
--- src/component/header/Genres.js
+++ src/component/header/Genres.js
@@ -1,72 +1,70 @@
 import React from "react";
 import "./header.css";
 
-
 function Genres(props) {
-    return (
-        <div className="choseType">
-            <div className="elementType">
-            <button class="element select">
-                <span>Action</span>
-            </button>
-            <button class="element">
-                <span>Aventure</span>
-            </button>
-            <button class="element">
-                <span>Animation</span>
-            </button>
-            <button class="element">
-                <span>Comédie Musicale</span>
-            </button>
-            <button class="element">
-                <span>Crime</span>
-            </button>
-            <button class="element">
-                <span>Documentaire</span>
-            </button>
-            <button class="element">
-                <span>Drame</span>
-            </button>
-            <button class="element">
-                <span>Famille</span>
-            </button>
-            <button class="element">
-                <span>Fantastique</span>
-            </button>
-            <button class="element">
-                <span>Film TV</span>
-            </button>
-            <button class="element">
-                <span>Guerre</span>
-            </button>
-            <button class="element">
-                <span>Horreur</span>
-            </button>
-            <button class="element">
-                <span>Mystérieux</span>
-            </button>
-            <button class="element">
-                <span>Populaire</span>
-            </button>
-            <button class="element">
-                <span>Histoire</span>
-            </button>
-            <button class="element">
-                <span>Romantique</span>
-            </button>
-            <button class="element">
-                <span>Science Fiction</span>
-            </button>
-            <button class="element">
-                <span>Thriller</span>
-            </button>
-            <button class="element">
-                <span>Western</span>
-            </button>
-            </div>
-        </div>
-    );
-  }
-  
-  export default Genres;
-  
+  return (
+    <div className="choseType">
+      <div className="elementType">
+        <button class="element select">
+          <span>Action</span>
+        </button>
+        <button class="element">
+          <span>Aventure</span>
+        </button>
+        <button class="element">
+          <span>Animation</span>
+        </button>
+        <button class="element">
+          <span>Comédie Musicale</span>
+        </button>
+        <button class="element">
+          <span>Crime</span>
+        </button>
+        <button class="element">
+          <span>Documentaire</span>
+        </button>
+        <button class="element">
+          <span>Drame</span>
+        </button>
+        <button class="element">
+          <span>Famille</span>
+        </button>
+        <button class="element">
+          <span>Fantastique</span>
+        </button>
+        <button class="element">
+          <span>Film TV</span>
+        </button>
+        <button class="element">
+          <span>Guerre</span>
+        </button>
+        <button class="element">
+          <span>Horreur</span>
+        </button>
+        <button class="element">
+          <span>Mystérieux</span>
+        </button>
+        <button class="element">
+          <span>Populaire</span>
+        </button>
+        <button class="element">
+          <span>Histoire</span>
+        </button>
+        <button class="element">
+          <span>Romantique</span>
+        </button>
+        <button class="element">
+          <span>Science Fiction</span>
+        </button>
+        <button class="element">
+          <span>Thriller</span>
+        </button>
+        <button class="element">
+          <span>Western</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Genres;
