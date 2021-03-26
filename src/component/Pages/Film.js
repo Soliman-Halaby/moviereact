d94acff9eae2adfe95c6a25b0c3774000522f948
Soliman Al Halaby <60599592+Soliman-Halaby@users.noreply.github.com>
2021-03-26 04:58:49 +0100

    :bug: Fix Header

--- src/component/Pages/Film.js
+++ src/component/Pages/Film.js
@@ -1,11 +1,12 @@
 import React from "react";
+// import Header from "../header/noheader";
 import Filmdisplay from "../VideoDisplay/Filmdisplay";
 import "./Film.css";
 // import video1 from "../../assets/videos/banana.mp4";
 import image from "../../assets/images/image.png";
 import play from "../../assets/logos/play.svg";
 
-function Film(props) {
+function Film() {
   // console.log(useGetFilms);
   // console.log(this.props.movieName);
   const About = (props) => {
@@ -13,6 +14,7 @@ function Film(props) {
   };
   return (
     <div className="containerFilm">
+      {/* <Header /> */}
       <Filmdisplay />
     </div>
   );
