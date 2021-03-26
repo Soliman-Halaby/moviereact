5c9097e485cc3b46c8e8e296305d6675d1a71e7e
Soliman Al Halaby <60599592+Soliman-Halaby@users.noreply.github.com>
2021-03-26 07:10:26 +0100

    :rocket: Add External Link

--- src/component/VideoDisplay/Filmdisplay.js
+++ src/component/VideoDisplay/Filmdisplay.js
@@ -4,13 +4,18 @@ import { NavLink } from "react-router-dom";
 import ArrowLeft from "../../assets/images/arrowLeft.svg";
 import Rating from "../Rating/Rating";
 import "./Filmdisplay.css";
-
+import extLinkImg from "../../assets/images/externalLinkImg.svg";
 export default function Filmdisplay(props) {
   return (
     <div className="filmDisplay">
       <NavLink to="/" className="headerTabs">
         <img className="arrowLeft" alt="Arrow Left" src={ArrowLeft}></img>
       </NavLink>
+      <div className="externalLink">
+        <a href={props.urlSrc} target="_blank">
+          <img src={extLinkImg}></img>
+        </a>
+      </div>
       <img className="filmImage" alt="Film Image" src={props.src}></img>
       <div className="filmInformation">
         <span className="pageFilmTitle">{props.title}</span>
@@ -22,14 +27,12 @@ export default function Filmdisplay(props) {
           <span className="pageFilmDate">{props.releasedYear}</span>
           <span className="pageFilmDuration">{props.runtime} minutes</span>
         </div>
-        <span className="pageFilmDescription">
-          {props.overview}
-        </span>
+        <span className="pageFilmDescription">{props.overview}</span>
       </div>
     </div>
   );
 }
 
-
-
-{/* <a src={props.urlSrc}></a> */}
+{
+  /* <a src={props.urlSrc}></a> */
+}
